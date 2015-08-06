a017807be7ba58edd19fb6bcec7cf1010ce29141
Bastien Picharles <b.picharles@oxygene-tanukis.com>
2015-08-06 14:15:17 +0200

    rm vendor from repo + allow create new project deployment

--- web/index.php
+++ web/index.php
@@ -1,6 +1,10 @@
 <?php
 // web/index.php
 require_once __DIR__.'/../vendor/autoload.php';
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 define('RECIPES_FOLDER', __DIR__.'/../recipes');
 
 $app = new Silex\Application();
@@ -16,6 +20,11 @@ $app['recipes'] = function(){
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../templates', // The path to the templates, which is in our case points to /var/www/templates
 ));
+$app->register(new Silex\Provider\FormServiceProvider());
+$app->register(new Silex\Provider\TranslationServiceProvider(), array(
+    'translator.domains' => array(),
+));
+$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
 
 
 //accueil
@@ -69,7 +78,8 @@ $app->get('/projects/{name}', function (Silex\Application $app, $name)  {
     );
     
     return $output; // Return it to so it gets displayed by the browser
-});
+})
+->bind('project_edit');
 
 //action de mise à jour d'une recette
 $app->post('/ajax/updateRecipe', function (Silex\Application $app, Symfony\Component\HttpFoundation\Request $request)  {
@@ -92,6 +102,38 @@ $app->post('/ajax/deployStaging', function (Silex\Application $app, Symfony\Comp
     echo "<pre>$output</pre>";
     return $output;
     
+});
+
+$app->match('/project/new', function (Request $request) use ($app) {
+
+    $form = $app['form.factory']->createBuilder('form')
+        ->add('name', null, array('required' => true))
+        ->add('Create', 'submit')
+        ->getForm();
+
+    $form->handleRequest($request);
+    if ($form->isValid()) {
+        $data = $form->getData();
+        
+        $folder = RECIPES_FOLDER . '/' . $data['name'];
+        if (!file_exists($folder)) {
+            mkdir($folder);
+            $file = $folder . '/deploy.php';
+            file_put_contents($file, '<?php');
+        }
+
+        $url = $app['url_generator']->generate('project_edit', array(
+            'name' => $data['name']
+        ));
+        return $app->redirect($url);
+    }
+
+    return $app['twig']->render(
+        'project-new.html.twig', 
+        array(
+            'form' => $form->createView()
+        )
+    );
 });
 
 $app->run();
